f121428eaebe4a612eea93d62260f88c9a6a9199
Mexanik72 <andrew6363@mail.ru>
2016-04-02 22:36:31 +0300

    IMPL: YOUTUBE API

--- app.js
+++ app.js
@@ -4,22 +4,28 @@ var sqlite = require('sqlite3');
 app.set('view engine', 'ejs');
 var db = new sqlite.Database('mydb.sqlite');
 app.use(express.static(__dirname + '/public'));
+var http = require("http");
+var https = require("https");
 
 var menu;
-
+var googleData = ["das", "as", "dssa"];
 app.get('/', function(req, res){
-db.all('select * from categories', function (error,data){
+	db.all('select * from categories', function (error,data){
 		menu = data;
 	});
 	db.all('select * from content', function (error,data){
-		res.render('home', {
-			data:data,
-			menu:menu
-		});
+			res.render('home', {
+				data:data,
+				menu:menu
+			});
 	});
 });
-
-
+app.post('/remove', function(req, res){
+	db.run("delete from content where id ="+req.query.id, function (){
+		console.log("remove"+req.query.id)
+		res.send('ok');
+	})
+});
 app.listen(3000, function() {
 	console.log('Listening on port 3000');
 });
